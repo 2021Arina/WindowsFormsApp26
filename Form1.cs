dee67ceec463dc3a94df752935eecfeeb4c33f41
2021Arina <93840292+2021Arina@users.noreply.github.com>
2022-05-22 22:41:50 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿
+// 1. Расширьте приложение путем добавления возможности выбора
+// пользователем цвета и величины кисти. 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+
+            DoubleBuffered = true;
+
+        }
+
+        private Point PreviousPoint, point;
+        private Bitmap bmp;
+        private Pen blackPen;
+        private Graphics g;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.BMP, *.JPG, " +
+                "*.GIF, *.PNG)|*.bmp;*.jpg;*.gif;*.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(dialog.FileName);
+                int width = image.Width;
+                int height = image.Height;
+                pictureBox1.Width = width;
+                pictureBox1.Height = height;
+                bmp = new Bitmap(image, width, height);
+                pictureBox1.Image = bmp;
+                g = Graphics.FromImage(pictureBox1.Image);
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            blackPen = new Pen(Color.Black, 4);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            PreviousPoint.X = e.X;
+            PreviousPoint.Y = e.Y;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savedialog = new SaveFileDialog();
+            savedialog.Title = "Сохранить картинку как ...";
+            savedialog.OverwritePrompt = true;
+            savedialog.CheckPathExists = true;
+            savedialog.Filter =
+                "Bitmap File(*.bmp)|*.bmp|" +
+                "GIF File(*.gif)|*.gif|" +
+                "JPEG File(*.jpg)|*.jpg|" +
+                "PNG File(*.png)|*.png";
+            if (savedialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = savedialog.FileName;
+                string strFilExtn = fileName.Remove(0,
+                 fileName.Length - 3);
+                switch (strFilExtn)
+                {
+                    case "bmp":
+                        bmp.Save(fileName,
+                        System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case "jpg":
+                        bmp.Save(fileName,
+                       System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "gif":
+                        bmp.Save(fileName,
+                        System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                    case "tif":
+                        bmp.Save(fileName,
+                       System.Drawing.Imaging.ImageFormat.Tiff);
+                        break;
+                    case "png":
+                        bmp.Save(fileName,
+                        System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    int R = bmp.GetPixel(i, j).R;
+                    int G = bmp.GetPixel(i, j).G;
+                    int B = bmp.GetPixel(i, j).B;
+                    int Gray = (R + G + B) / 3;
+                    Color p = Color.FromArgb(255, Gray, Gray,
+                      Gray);
+                    bmp.SetPixel(i, j, p);
+                }
+            Refresh();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) // Изменение цвета
+        {
+            if (colorDialog1.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
+            {
+                blackPen.Color = colorDialog1.Color;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) // Выбор толщины кисти
+        {
+            if (comboBox1.SelectedIndex == 0)
+                blackPen = new Pen(Color.Black, 1);
+            if (comboBox1.SelectedIndex == 1)
+                blackPen = new Pen(Color.Black, 2);
+            if (comboBox1.SelectedIndex == 2)
+                blackPen = new Pen(Color.Black, 4);
+            if (comboBox1.SelectedIndex == 3)
+                blackPen = new Pen(Color.Black, 5);
+            if (comboBox1.SelectedIndex == 4)
+                blackPen = new Pen(Color.Black, 8);
+            if (comboBox1.SelectedIndex == 5)
+                blackPen = new Pen(Color.Black, 10);
+            if (comboBox1.SelectedIndex == 6)
+                blackPen = new Pen(Color.Black, 12);
+            if (comboBox1.SelectedIndex == 7)
+                blackPen = new Pen(Color.Black, 15);
+            if (comboBox1.SelectedIndex == 8)
+                blackPen = new Pen(Color.Black, 20);
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                point.X = e.X;
+                point.Y = e.Y;
+                g.DrawLine(blackPen, PreviousPoint, point);
+                PreviousPoint.X = point.X;
+                PreviousPoint.Y = point.Y;
+                pictureBox1.Invalidate();
+            }
+        }
+
+
+
+    }
+}
